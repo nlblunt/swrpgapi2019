4eea2aaa946f06507e767485257a97a1e773e7dc
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2018-01-17 11:12:57 -0800

    Initial devise_token_auth setup

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,15 @@ module SwrpgApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
+
+    config.middleware.use Rack::Cors do
+        allow do
+            origins '*'
+            resource '*',
+                :headers => :any,
+                :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                :methods => [:get, :post, :options, :delete, :put]
+            end
+        end
+    end
 end
