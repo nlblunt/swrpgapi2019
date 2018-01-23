4a8cf549b8e6b2c56548775849ad02f844a4e1d9
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2018-01-23 10:46:17 -0800

    Devise Token API

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,6 @@
 class Player < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 end
