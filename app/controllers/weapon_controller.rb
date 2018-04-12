da9ee74825e6a7ae02a4dd63ae0c67b5468fd0ab
Nicolas <nicolas.blunt@gmail.com>
2018-04-11 17:41:13 -0700

    Weapon Update
    GM can update weapon.

--- app/controllers/weapon_controller.rb
+++ app/controllers/weapon_controller.rb
@@ -9,8 +9,21 @@ class WeaponController < ApplicationController
   end
 
   def update
+	@weapon = Weapon.find_by_id(params[:id])
+
+	#Update the weapon
+	if @weapon.update(weapon_params)
+		head :ok
+	else
+		head :error
+	end
   end
 
   def destroy
+  end
+
+  private
+  def weapon_params
+	params.require(:weapon).permit(:name, :skill_id, :damage, :critical, :range, :price, :special, :description, :notes)
   end
 end
